eb699b93764482d721c4c69e5b8203e96afe2f27
maria <marshellundikalibra171005@gmail.com>
2023-01-11 13:15:20 +0600

    initial commit .

---
+++ git_commands.py
@@ -0,0 +1,10 @@
+git init- .git
+git remote add name url
+git pull origin master
+git status
+git add
+git commit -m "comment"
+git branch
+git branch name branch
+git push origin master
+git reset filename
